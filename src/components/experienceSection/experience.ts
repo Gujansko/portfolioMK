5fb34482832e9a638a88df4a81e9348f9e9dc4db
Gujansko <mateusz.klos150401@gmail.com>
2023-08-12 02:13:52 +0200

    Experience text changes

--- src/components/experienceSection/experience.ts
+++ src/components/experienceSection/experience.ts
@@ -12,7 +12,7 @@ const Idemia: experience = {
   position: "Frontend Developer Intern",
   date: "03/2023 - 06/2023",
   description:
-    "During my time at the company, I had a great opportunity to feel what it's like to be part of a team and work under time pressure. As it was only the first time I had a chance to work on a real product I tried to make the most of it and learn as much as possible in terms of soft and software skills. My responsibilites where centered around developing quiz flow. Which means displaying it, answering and handling responses. I also validated and worked on establishing connection between frontend and backend regarding quizes. Our team was able to develop a web application with quizes for internal company use. The app was also successfully used at the company's expo by hundreds of visitors.",
+    "During my time at the company, I had a great opportunity to feel what it's like to be part of a team and work under time pressure. As it was only the first time I had a chance to work on a real product I tried to make the most of it and learn as much as possible in terms of soft and software skills. My responsibilites were writing component tests and creating quiz flow. What means displaying it, answering and handling responses. I was also establishing and validating connections  regarding quizes between frontend and backend. Our team was able to develop a web application with quizes for internal company use. The app was also successfully used at the company's expo by hundreds of visitors.",
   backgroundColor: "before:bg-[#420098]",
   underlineColor: "group-hover:decoration-[#420098]",
 };
